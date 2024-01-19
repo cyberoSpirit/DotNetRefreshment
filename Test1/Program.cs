e162c8215e6a088a803d2ff7f159703f9b98bafe
Julia <julia.hrytsyk@gmail.com>
2024-01-19 23:08:06 +0100

    Added Computer model.

---
+++ Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Test1.Models;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "RTX 2060"
+            };
+            myComputer.HasWifi = false;
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.HasWifi);
+            Console.WriteLine(myComputer.ReleaseDate);
+            Console.WriteLine(myComputer.VideoCard);
+        }
+
+    }
+}
